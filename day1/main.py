1b08221992c712ddad7c619ad49876a511355b81
kohlakos <76475497+kohlakos@users.noreply.github.com>
2022-12-21 15:18:30 +0100

    calc ready, next sort

--- day1/main.py
+++ day1/main.py
@@ -2,11 +2,13 @@ elf_number = 0
 max_cal = 0
 with open("input.txt") as cal_file:
     
-    test = 0
+    temp = 0
     for cal in cal_file.readlines():
         
         if cal == "\n":
-            print(test)
+            max_cal = temp
+            print(temp)
+            temp = 0
         else:
-            test += int(cal.rstrip())
+            temp += int(cal.rstrip())
 
