fd3e689294412af0c072719e5b02ef6560c4e1d3
kohlakos <76475497+kohlakos@users.noreply.github.com>
2022-12-22 17:39:48 +0100

    creating test input from example

--- day2/main.py
+++ day2/main.py
@@ -6,35 +6,34 @@
 totalpoints = 0
 with open("input.txt") as gamefile:
 
-    roundpoint = 0
+    my_points = 0
+    opp_points = 0
     for index, game in enumerate(gamefile.readlines(), 1):
         opponent = game[0]
         me = game[2]
         
         if opponent == "A" and me == "X":
             #draw
-            roundpoint += 3
+            my_points += 3
+            opp_points += 3
         
         if opponent == "A" and me == "Y":
-            roundpoint += 6
-        
+            my_points += 6
         if opponent == "B" and me == "Z":
-            roundpoint += 6
-
+            my_points += 6
         if opponent == "C" and me == "X":
-            roundpoint += 6
+            my_points += 6
 
         if me == "X":
             roundpoint += 1
-
         if me == "Y":
             roundpoint += 2
-
         if me == "Z":
             roundpoint += 3
 
         if index % 3 == 0 :
             #end of every round
+            if 
             totalpoints += roundpoint
             roundpoint = 0
 
