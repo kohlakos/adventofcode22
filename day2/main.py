fdf1dd32e482bd3ee4d863470b8499344f8b90ac
kohlakos <76475497+kohlakos@users.noreply.github.com>
2022-12-22 17:26:37 +0100

    wrong understanding of the task, point was given after every match, not after every round...

--- day2/main.py
+++ day2/main.py
@@ -1,22 +1,38 @@
 # A for Rock, B for Paper, and C for Scissors.
 # X for Rock, Y for Paper, and Z for Scissors.
 # Additional points: 1 for Rock, 2 for Paper, and 3 for Scissors.
-# Drawn is 3 win is 6.
+# Draw is 3 win is 6.
 
 totalpoints = 0
 with open("input.txt") as gamefile:
 
     roundpoint = 0
-    for index, game in enumerate(gamefile.readlines()):
+    for index, game in enumerate(gamefile.readlines(), 1):
         opponent = game[0]
         me = game[2]
         
         if opponent == "A" and me == "X":
+            #draw
             roundpoint += 3
         
         if opponent == "A" and me == "Y":
-            roundpoint += 3
+            roundpoint += 6
         
+        if opponent == "B" and me == "Z":
+            roundpoint += 6
+
+        if opponent == "C" and me == "X":
+            roundpoint += 6
+
+        if me == "X":
+            roundpoint += 1
+
+        if me == "Y":
+            roundpoint += 2
+
+        if me == "Z":
+            roundpoint += 3
+
         if index % 3 == 0 :
             #end of every round
             totalpoints += roundpoint
