c158676bf76ef6ea7c7e3056500f56773c2eaf98
kohlakos <76475497+kohlakos@users.noreply.github.com>
2022-12-22 17:11:15 +0100

    char separation, end of round points, some if

--- day2/main.py
+++ day2/main.py
@@ -1,6 +1,25 @@
+# A for Rock, B for Paper, and C for Scissors.
+# X for Rock, Y for Paper, and Z for Scissors.
+# Additional points: 1 for Rock, 2 for Paper, and 3 for Scissors.
+# Drawn is 3 win is 6.
+
 totalpoints = 0
 with open("input.txt") as gamefile:
 
-    for game, index in enumerate(gamefile.readlines()):
-        print(index, game)
-       
+    roundpoint = 0
+    for index, game in enumerate(gamefile.readlines()):
+        opponent = game[0]
+        me = game[2]
+        
+        if opponent == "A" and me == "X":
+            roundpoint += 3
+        
+        if opponent == "A" and me == "Y":
+            roundpoint += 3
+        
+        if index % 3 == 0 :
+            #end of every round
+            totalpoints += roundpoint
+            roundpoint = 0
+
+print(totalpoints)
