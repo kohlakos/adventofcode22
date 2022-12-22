a53d3e33621765e8556442ab5b078e4e9181a106
kohlakos <76475497+kohlakos@users.noreply.github.com>
2022-12-22 18:07:24 +0100

    part 2 new file

---
+++ day2/main2.py
@@ -0,0 +1,42 @@
+#X means you need to lose, 
+#Y means you need to end the round in a draw
+#Z means you need to win.
+
+totalpoints = 0
+with open("input.txt") as gamefile:
+
+    roundpoint = 0
+    for index, game in enumerate(gamefile.readlines()):
+        opponent = game[0]
+        me = game[2]
+
+        #draw
+        if opponent == "A" and me == "X":
+            roundpoint += 3
+        if opponent == "B" and me == "Y":
+            roundpoint += 3
+        if opponent == "C" and me == "Z":
+            roundpoint += 3
+        
+        #win
+        if opponent == "A" and me == "Y":
+            roundpoint += 6
+        if opponent == "B" and me == "Z":
+            roundpoint += 6
+        if opponent == "C" and me == "X":
+            roundpoint += 6
+
+        #extra points
+        if me == "X":
+            roundpoint += 1
+        if me == "Y":
+            roundpoint += 2
+        if me == "Z":
+            roundpoint += 3
+
+        #end of every round, actually that doesnt matter, first I tought it would...
+        if index % 3 == 0 :
+            totalpoints += roundpoint
+            roundpoint = 0
+
+print(totalpoints)
