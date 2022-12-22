1f653f2c01e31f40461848b0f2db7a6c3db3df1e
kohlakos <76475497+kohlakos@users.noreply.github.com>
2022-12-22 17:47:07 +0100

    forgot to create all draw scenario, task understood, example calc ok

--- day2/main.py
+++ day2/main.py
@@ -6,23 +6,28 @@
 totalpoints = 0
 with open("test.txt") as gamefile:
 
-    my_points = 0
-    opp_points = 0
+    roundpoint = 0
     for index, game in enumerate(gamefile.readlines(), 1):
         opponent = game[0]
         me = game[2]
-        
+        print(roundpoint)
+
         if opponent == "A" and me == "X":
             #draw
-            my_points += 3
-            opp_points += 3
+            roundpoint += 3
+        if opponent == "B" and me == "Y":
+            #draw
+            roundpoint += 3
+        if opponent == "C" and me == "Z":
+            #draw
+            roundpoint += 3
         
         if opponent == "A" and me == "Y":
-            my_points += 6
+            roundpoint += 6
         if opponent == "B" and me == "Z":
-            my_points += 6
+            roundpoint += 6
         if opponent == "C" and me == "X":
-            my_points += 6
+            roundpoint += 6
 
         if me == "X":
             roundpoint += 1
@@ -33,7 +38,6 @@ with open("test.txt") as gamefile:
 
         if index % 3 == 0 :
             #end of every round
-            if 
             totalpoints += roundpoint
             roundpoint = 0
 
