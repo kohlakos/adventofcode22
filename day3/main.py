8c2ce76f911add57079657666c5bc70a4ac73c5f
kohlakos <76475497+kohlakos@users.noreply.github.com>
2022-12-24 15:35:58 +0100

    removing testing trash

--- day3/main.py
+++ day3/main.py
@@ -13,12 +13,5 @@ def load_input():
         first_comp = rucksack[0:divider]
         second_comp = rucksack[divider:length]
 
-        for letter, count in itemlist.items():
-            if (count > 1):
-                print(letter)
-
-
 if __name__ == "__main__":
     input = load_input()
-    for lines in input:
-        find_dup_char(lines)
